9d49234bf7bd609a90edd2c8723a70bc11875246
Dynamite4090 <chavanganesh9405543605@gmail.com>
2024-10-01 14:50:50 +0530

    gt

---
+++ 2D_Monks_Reveng/Assets/winning.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextscene : MonoBehaviour
+{
+    // Name of the next scene to load
+    public string nextSceneName;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the object colliding has the tag "Player"
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Check if this object has the tag "Finish"
+            if (gameObject.CompareTag("Finish"))
+                Debug.Log("Game run");
+            {
+                // Load the next scene
+                SceneManager.LoadScene(nextSceneName);
+
+
+            }
+        }
+    }
+}
